361d4f6c0eb4682c23ab8c6d9dcf290086f3d7b5
hrk2023 <hrkdasgupta025@gmail.com>
2020-10-17 09:15:13 -0700

    CORS add attempt #5

--- FreeNetflix/__init__.py
+++ FreeNetflix/__init__.py
@@ -2,11 +2,10 @@ from flask import Flask, Blueprint
 from flask_cors import CORS
 from .routes import userRoutes, adminRoutes, applicationRoutes
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, origins='*')
 app.config["SECRET_KEY"] = "dAmNSimPLeSecREtKeY"
-app.config["CORS_ORIGIN_WHITELIST"] = ["*"]
 
-origins = app.config.get('CORS_ORIGIN_WHITELIST', '*')
+origins = '*'
 cors.init_app(userRoutes.bp, origins=origins)
 cors.init_app(adminRoutes.admin, origins=origins)
 cors.init_app(applicationRoutes.bp, origins=origins)
