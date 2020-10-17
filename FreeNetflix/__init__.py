30741892fedb2375fc760054c317d03f1ed985d8
hrk2023 <hrkdasgupta025@gmail.com>
2020-10-17 07:47:59 -0700

    Flask Cors Added

--- FreeNetflix/__init__.py
+++ FreeNetflix/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from .routes import userRoutes, adminRoutes, applicationRoutes
 app = Flask(__name__)
-
+cors = CORS(app)
 app.config["SECRET_KEY"] = "dAmNSimPLeSecREtKeY"
 app.register_blueprint(userRoutes.bp)
 app.register_blueprint(adminRoutes.admin)
